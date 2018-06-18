f531d37600f35e7ec983b7bf81d1fe907a14d17f
Fx Morin <fx.e.morin@gmail.com>
2018-06-18 15:00:15 -0400

    Added outputs
    Added outputs for send() & sendArray()

--- lineUs.php
+++ lineUs.php
@@ -13,28 +13,34 @@ function send($gcode,$addr= "line-us.local", $port = "1337") {
   $client = @stream_socket_client("tcp://$addr:$port", $errno, $errorMessage);
   stream_set_timeout($client, 1);
   if($client === false){die("Unable to communicate to bot!");}
+  $output = "";
   $gcode = str_replace("\n","\0\r",$gcode);
   $array = explode( "\r", $gcode );
   $lines = SplFixedArray::fromArray($array);
   unset( $array );
     foreach ($lines as $line) {
-      if (readOutput($client)) {fwrite($client, $line);}
+      $output .= readOutput($client);
+      if ($output != "") {fwrite($client, $line);}
     }
   fwrite($client, "\0");
   fclose($client);
+  return $output;
 }
 function sendArray($gcode,$addr= "line-us.local", $port = "1337") {
   if ($addr == "") {die("Unable to find line-us bot!");}
   $client = @stream_socket_client("tcp://$addr:$port", $errno, $errorMessage);
   stream_set_timeout($client, 1);
   if($client === false){die("Unable to communicate to bot!");}
+  $output = "";
   $lines = SplFixedArray::fromArray($gcode);
   unset( $gcode );
     foreach ($lines as $line) {
-      if (readOutput($client)) {fwrite($client, $line);}
+      $output .= readOutput($client);
+      if ($output != "") {fwrite($client, $line);}
     }
   fwrite($client, "\0");
   fclose($client);
+  return $output;
 }
 function getIp($port = "1337") {
   ignore_user_abort(true);     ## prevent refresh from aborting file operations and hosing file
